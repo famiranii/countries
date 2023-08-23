6703a02971e9dbdf9eb1ee6b486795d7af748b23
farhad40 <you@example.com>
2023-08-23 21:21:07 +0330

    home page is responsive

--- src/countries/Header.js
+++ src/countries/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <header className='header'>
-      <h1>Where in the world</h1>
+      <h1 className='title'>Where in the world</h1>
       <p>Dark Mode</p>
     </header>
   )
